75de1bb1dc8d496776284095db8b07f497113ae0
Jyotiraditya Yadav <95582292+jyotiradityadav@users.noreply.github.com>
2025-06-10 20:30:52 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,20 +1,23 @@
-## Function Documentation
+def fibonacci(n):
+    a = 0
+    b = 1
+    
+    # Check if n is less than 0
+    if n < 0:
+        print("Incorrect input")
+        
+    # Check if n is equal to 0
+    elif n == 0:
+        return 0
+      
+    # Check if n is equal to 1
+    elif n == 1:
+        return b
+    else:
+        for i in range(1, n):
+            c = a + b
+            a = b
+            b = c
+        return b
 
-### Overview
-This function performs the specified operation on the input data.
-
-### Parameters
-- `param1`: Description of first parameter
-- `param2`: Description of second parameter
-
-### Returns
-Description of return value
-
-### Example
-```python
-result = function_name(param1, param2)
-```
-
-### Notes
-- Important note 1
-- Important note 2
+print(fibonacci(9))
