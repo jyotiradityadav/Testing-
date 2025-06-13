bc79bbb294b7ddaec9418d2785e9d5f04e7cdd05
Jyotiraditya Yadav <95582292+jyotiradityadav@users.noreply.github.com>
2025-06-13 14:01:54 +0530

    Update code: sorting_algorithms/helloworld.py

--- sorting_algorithms/helloworld.py
+++ sorting_algorithms/helloworld.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 def bubble_sort(arr):
+    """
+    Sorts an array using the bubble sort algorithm.
+
+    Time Complexity: O(n^2)
+    Space Complexity: O(1)
+
+    :param arr: List of elements to be sorted
+    :return: Sorted list
+    """
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
@@ -9,6 +18,15 @@ def bubble_sort(arr):
     return arr
 
 def quick_sort(arr):
+    """
+    Sorts an array using the quick sort algorithm.
+
+    Time Complexity: O(n log n) on average
+    Space Complexity: O(n)
+
+    :param arr: List of elements to be sorted
+    :return: Sorted list
+    """
     if len(arr) <= 1:
         return arr
     pivot = arr[len(arr) // 2]
